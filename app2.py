0e4b3d50bc04627efa4099db0a21b8c56e442808
Xiao Fu <lemonaddie@users.noreply.huggingface.co>
2024-03-23 14:04:19 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -53,7 +53,7 @@ sd_image_variations_diffusers_path = '.'
 image_encoder = CLIPVisionModelWithProjection.from_pretrained(sd_image_variations_diffusers_path, subfolder="image_encoder")
 feature_extractor = CLIPImageProcessor.from_pretrained(sd_image_variations_diffusers_path, subfolder="feature_extractor")
 
-unet = UNet2DConditionModel.from_pretrained('tbd')
+unet = UNet2DConditionModel.from_pretrained('./wocfg/unet_ema')
 
 pipe = DepthNormalEstimationPipeline(vae=vae,
                             image_encoder=image_encoder,
